2025f915a9b696d4ca11cede28d071bc44885c08
HHyeseon <contedete15@gmail.com>
2019-12-13 21:53:21 +0900

    1016-(2)

--- src/bj1016/Main.java
+++ src/bj1016/Main.java
@@ -6,20 +6,40 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer t = new StringTokenizer(r.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		long min=Long.parseLong(t.nextToken());
 		long max=Long.parseLong(t.nextToken());
-		r.close();
 		
-		boolean[]a = new boolean[(int)(max-min+1)];
-		for(int i=2; true; ++i) {
-			long square=i*i;
+
+		boolean[]check = new boolean[(int)(max-min+1)];
+		for(int i=2; i*i<=max; ++i) {
+			long square=i*i;		//제곱수
 			if(square>max)
 				break;
+			long s= min%square==0? min/square : (min/square)+1;  //min보다 큰 제곱수 중에서 시작값을 구함
 			
+			while(s<min)
+				s+=square;
+			
+			while(s<=max) {
+				check[(int)(max-s)] = true;
+				s+=square;
+			}
+			
+			int count=0;
+			for(int j=(int)min; j<max; ++j) {
+				if(!check[j])
+					count++;
+			}
+			bw.flush();
+			bw.write(count+" ");
+			br.close();
+			bw.close();
 		}
-		
+	
+
 	}
 
 }
