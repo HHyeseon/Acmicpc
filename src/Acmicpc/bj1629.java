3d96fc03c6e009bafcf67da55572f91e8fcfb2ac
HHyeseon <contedete15@gmail.com>
2019-12-16 23:49:44 +0900

    1629-(3)

--- src/Acmicpc/bj1629.java
+++ src/Acmicpc/bj1629.java
@@ -6,26 +6,26 @@ public class bj1629 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner input=new Scanner(System.in);
-		int a=input.nextInt();
-		int b=input.nextInt();
-		int mod=input.nextInt();
-		int result=1;
-		int mul=(a%mod);
+		Scanner input = new Scanner(System.in);
+		long a=input.nextInt();
+		long b=input.nextInt();
+		long mod=input.nextInt();
+		
+		long result=1;
+		long mul=(a%mod);
 		
 		while(b>0) {
-			if(b%2 == 1) {
+			if(b%2==1) {
 				result *= mul;
 				result %= mod;
-				
-			} 
-			mul= ((mul%mod)*(mul%mod))%mod;
-			//mul=(mul*mod)%mod;
-		
+			}
+			
+			mul=((mul%mod)*(mul%mod))%mod;
+			//mul=((mul%mod)*(mul%mod));
+			//mul=(mul*mod)%mod;      
 			b/=2;
 		}
 		System.out.print(result);
-		
 		
 	}
 }
