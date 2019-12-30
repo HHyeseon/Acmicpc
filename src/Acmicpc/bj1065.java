64814d11c2be8c65a2dbfaf0ada01b39eebaaaaa
HHyeseon <contedete15@gmail.com>
2019-12-30 21:20:03 +0900

    1065-2

--- src/Acmicpc/bj1065.java
+++ src/Acmicpc/bj1065.java
@@ -9,9 +9,10 @@ public class bj1065 {
 		int b= (number/10)%10;		 //십의 자리
 		int c= (number/100)%10;   //백의 자리
 		
-		
-		
-		if(2*b == a+c)
+		int gap1=c-b;
+		int gap2=b-a;
+			
+		if(gap1==gap2)
 			return 1;
 		
 		
