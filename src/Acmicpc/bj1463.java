86af41a5e51acc7fbc32393615ca1b8d52d10a18
HHyeseon <contedete15@gmail.com>
2020-02-16 18:58:12 +0900

    bj1463_TOPDOWN

--- src/Acmicpc/bj1463.java
+++ src/Acmicpc/bj1463.java
@@ -3,30 +3,44 @@ package Acmicpc;
 import java.util.*;
 public class bj1463 {
 
-	public static void main(String[] args)  {
-		// TODO Auto-generated method stub
-		Scanner input= new Scanner(System.in);
-		long n = input.nextLong();
-		long[]arr = new long[(int)n+1];
-
-		if(n<=1) {			//n이 1보다 작거나 같을 경우는 계산할 필요x
-			arr[0]=0;
-			arr[1]=0;
+		static int[]arr;
+		public static void main(String[] args)  {
+			Scanner input=new Scanner(System.in);
+			int n=input.nextInt();
+			arr=new int[n+1];
+			
+			
+			System.out.print(one(n));
 		}
 
-		for(int i=2; i<=n; ++i) {		//조건에 해당하면 배열에 1을 더한다
-			arr[(int)i]= arr[(int)i-1]+1;
 
-			if(n%3==0 && arr[(int)i/3]+1 < arr[(int)i])
-				arr[(int)i] = arr[(int)i/3]+1;
-			else if(n%2==0 && arr[(int)i/2]+1 < arr[(int)i])
-				arr[(int)i] = arr[(int)i/2]+1;
 
+
+
+		public static int one(int n) {
+
+		
+			if(n<=1) 
+				return 0;
+			if(arr[n]>0)
+				return arr[n];
+			
+			
+			int temp= one(n-1)+1;
+			if(n%2==0) {
+				arr[n]= one(n/2)+1;
+				if(temp>arr[n])
+					temp=arr[n];
+			}
+			else if(n%3==0) {
+				arr[n]= one(n/3)+1;
+				if(temp>arr[n])
+					temp=arr[n];
+			
+			}
+			
+			return temp;
 		}
-
-		System.out.print(arr[(int)n]);
-
-
-
 	}
-}
+
+
